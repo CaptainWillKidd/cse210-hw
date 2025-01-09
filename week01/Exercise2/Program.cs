f53be499a5170761007596e7b2ab18c30adede9d
CaptainWillKidd <can24018@byui.edu>
2025-01-09 20:30:13 -0300

    Exercise 2 Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Please, enter your grade in numbers: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the course! Congratulation! Continue the good job!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course! Don't give up! Try again! We will help you!");
+        }
     }
 }
